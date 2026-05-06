cc90d308393c8d545ec288fae6868bb7bcc21f65
Lushichen <1263868407@qq.com>
2026-05-06 15:38:48 +0800

    feat(example): 添加网易云音乐API示例代码
    添加示例代码展示如何使用Meting库与网易云音乐API交互，包括搜索歌曲、获取歌曲信息、图片和歌词等功能

--- example/meting_dart_example.dart
+++ example/meting_dart_example.dart
@@ -1,3 +1,21 @@
 import 'package:meting_dart/meting_dart.dart';
 
-void main() {}
+void main() async {
+  await neteaseExample();
+}
+
+Future<void> neteaseExample() async {
+  final meting = Meting(server: 'netease')..format(true);
+  final searchFuture = await meting.search('晚安-颜人中', option: {'limit': 10});
+  print(searchFuture);
+  print("-----------------");
+  final song = await meting.song('1359356908');
+  print(song);
+  print("-----------------");
+  final picture = await meting.pic('109951170473693123');
+  print(picture);
+  print("-----------------");
+  final lyric = await meting.lyric('2731571357');
+  print(lyric);
+  print("-----------------");
+}
