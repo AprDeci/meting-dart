c1727f3d70fa571d888ebd48c5bba0d8d6ad10c5
Lushichen <1263868407@qq.com>
2026-05-06 16:53:33 +0800

    feat(example): 添加酷狗音乐平台的示例代码
    添加了酷狗音乐平台的示例代码，展示如何使用Meting库进行搜索、获取歌曲信息、歌词和封面图

--- example/meting_dart_example.dart
+++ example/meting_dart_example.dart
@@ -2,10 +2,14 @@ import 'package:meting_dart/meting_dart.dart';
 
 void main() async {
   await neteaseExample();
+  await kugouExample();
 }
 
 Future<void> neteaseExample() async {
   final meting = Meting(server: 'netease')..format(true);
+  print(
+    "-------------------------------Netease Example----------------------------------",
+  );
   final searchFuture = await meting.search('晚安-颜人中', option: {'limit': 10});
   print(searchFuture);
   print("-----------------");
@@ -17,5 +21,24 @@ Future<void> neteaseExample() async {
   print("-----------------");
   final lyric = await meting.lyric('2731571357');
   print(lyric);
+  print("-----------------");
+}
+
+Future<void> kugouExample() async {
+  final meting = Meting(server: 'kugou')..format(false);
+  print(
+    "-------------------------------Kugou Example----------------------------------",
+  );
+  final result = await meting.search('Love Like This', option: {'limit': 3});
+  print(result);
+  print("-----------------");
+  final song = await meting.song('ec8eb73e83c6e2a2b58b55a3a21c1296');
+  print(song);
+  print("-----------------");
+  final lyric = await meting.lyric('ec8eb73e83c6e2a2b58b55a3a21c1296');
+  print(lyric);
+  print("-----------------");
+  final picture = await meting.pic('ec8eb73e83c6e2a2b58b55a3a21c1296');
+  print(picture);
   print("-----------------");
 }
